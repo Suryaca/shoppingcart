710204c1c44c870ba7af84c7b36b970d8be83486
Surya C. A <suryac.akasam@gmail.com>
2015-11-17 15:18:03 +0530

    Added new Way of writing the code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
+// Way One
+
+var cart = function(){
+  this.items = [];
+  this.price = 0;
+  this.removeFromCartById = function(id){
+    for(var i=0;i<this.items.length;i++){
+      if(this.items[i].id==id)
+        this.items.splice(i,1);
+    }
+  };
+  this.removeFromCartByName = function(name){
+    for(var i=0;i<this.items.length;i++){
+      if(this.items[i].name==name)
+        this.items.splice(i,1);
+    }
+  };
+  this.addToCart = function(product){
+    this.items.push(product);
+    this.price+=product.price;
+  };
+
+};
+cartObject = new cart();
+console.log(cartObject.price);
+var product = function(id, name, description, price){
+  this.id = id;
+  this.name =name;
+  this.description = description;
+  this.price = price;
+  this.addToCart = function(){
+    cartObject.items.push(this);
+    cartObject.price+=this.price;
+  };
+};
+
+var laptop = new product(1, "Dell","Dell 128gb SSD 4GB RAM",2000);
+laptop.addToCart();
+//console.log(cartObject);
+laptop.addToCart();
+laptop.addToCart();
+laptop.addToCart();
+//using cart object method
+cartObject.addToCart(laptop);
+console.log(cartObject.items.length);
+cartObject.removeFromCartById(1);
+console.log(cartObject.items);
+cartObject.removeFromCartById(1);
+console.log(cartObject.items);
+cartObject.removeFromCartByName("Dell")
+console.log(cartObject.items);
+
+/*
+
+// Way Two
+
 var cart = function(){
   this.items = [];
   this.price = 0;
@@ -40,3 +96,4 @@ cartObject.removeFromCartById(1);
 console.log(cartObject.items);
 cartObject.removeFromCartByName("Dell")
 console.log(cartObject.items);
+/*
